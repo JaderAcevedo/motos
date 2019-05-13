930e10420c1334107f757f999f1889b11fb4935a
JADER acevedo <jhonjader11@hotmail.com>
2019-05-13 09:32:30 -0500

    Se agrego el modelo venta

---
+++ Motos_Motores/Models/Venta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Motos_Motores.Models
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public String Cliente { get; set; }
+
+        public String Producto { get; set; }
+
+        public String Cantidad { get; set; }
+
+        public int Total { get; set; }
+    }
+}
